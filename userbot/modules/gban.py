ab7bedf610480412b197d51f98cf1b7c64f4a419
Kyuraxs <96177992+Kyuraxs@users.noreply.github.com>
2022-01-10 01:45:52 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -52,7 +52,7 @@ async def gben(userbot):
     else:
         dark = await dc.edit("`Memproses Global Banned Jamet..`")
     me = await userbot.client.get_me()
-    await dark.edit("`Global Banned Akan Segera Aktif..`")
+    await dark.edit("`Global Banned Akan Segera Aktif Kyura🥰..`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     await userbot.get_chat()
     a = b = 0
